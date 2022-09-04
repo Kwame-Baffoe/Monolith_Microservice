24e21f93709ef31490b484b0d5a66dbf14f6cf06
Kwame-Baffoe <111678522+Kwame-Baffoe@users.noreply.github.com>
2022-09-04 17:32:25 +0000

    Final Project Commit

---
+++ set_env.sh
@@ -0,0 +1,14 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+
+ export POSTGRES_USERNAME: adevabraham
+ export POSTGRES_PASSWORD: bv2000gee4
+ export POSTGRES_HOST: dev-instance.csxkksxsktt9.us-east-2.rds.amazonaws.com
+ export POSTGRES_DB: postgres
+ export AWS_BUCKET: abrahambucket45
+ export AWS_REGION: us-east-2
+ export AWS_PROFILE=default
+ export JWT_SECRET: testing
+ export URL: http://localhost:8100
+
+you have to add ur git to my terminal, i think it is signed to mine
